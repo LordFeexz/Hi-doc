6c8bbf2e35dbf3263a5c6f4e9c4772a08934fdc7
LordFeexz <anandafiqri1403@gmail.com>
2022-09-16 01:22:51 +0700

    ngerjai controller

--- controllers/medicineController.js
+++ controllers/medicineController.js
@@ -1,23 +1,34 @@
+const {Medicine,Disease,User} = require("../models")
+
 class Controller{
 
     static addMedicine(req,res){
-
+      Medicine.findAll({
+        include:[Disease,User]
+      })
+      .then(medicine => res.render('addMedicine',{ medicine }))
+      .catch(err => res.send(err))
     }
 
     static saveMedicine(req,res){
-
-    }
-
-    static medicineDetail(req,res){
-
+        const {name,price,stock,DiseaseId,UserId} = req.body
+        Medicine.create({
+            name:name,
+            price:price,
+            stock:stock,
+            DiseaseId:DiseaseId,
+            UserId:UserId
+        })
+        .then(res.redirect(`/medicine/`))
+        .catch(err => res.send(err))
     }
 
     static update(req,res){
-
+        
     }
 
     static saveChanges(req,res){
-        
+
     }
 }
 module.exports = Controller
